767b9e227a9e33f1ec965320d1194f4e9dd5ddd0
Fran <fmjerez@icloud.com>
2022-01-15 21:18:01 +0100

    Second commit

---
+++ pages/tienda.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Tienda = () => {
+  return (
+    <div>
+      <Layout pagina="Tienda">
+        <h1>Desde tienda</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Tienda;
